24dd407d9aa6faa0d446c7cea6da8aee278733e6
Mubarek Shikur <manmuba90@gmail.com>
2023-09-10 18:10:34 +0300

    Icons

--- app/components/navigation/Logo.tsx
+++ app/components/navigation/Logo.tsx
@@ -1,9 +1,23 @@
-'use client';
+"use client";
+
+import { LuSearch } from "react-icons/lu";
 
 const Logo = () => {
-    return (
-        <h6>Logo</h6>
-    );
-}
- 
+  return (
+    <div className="flex flex-row items-center gap-5">
+      <h6 className="flex items-center font-extrabold text-sky-600">Logo</h6>
+      <div className="hidden flex-row items-center md:flex ">
+        <input
+          type="text"
+          className="p-1 bg-neutral-200 rounded-full w-[23vw]"
+        />
+        <LuSearch 
+            className="font-extrabold text-orange-500 cursor-pointer relative right-8"
+            size="24"
+        />
+      </div>
+    </div>
+  );
+};
+
 export default Logo;
--- app/components/navigation/Navbar.tsx
+++ app/components/navigation/Navbar.tsx
@@ -6,6 +6,15 @@ const Navbar = () => {
     return ( 
         <div className="border-b-[1px] p-3 md:p-5 flex flex-row items-center justify-between">
             <Logo />
+            <div className="hidden flex-row md:flex gap-4 relative font-semibold text-neutral-700 right-[10vw] lg:gap-6">
+                <h6>Home</h6>
+                <h6>Home</h6>
+                <h6>Home</h6>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+                <div className="flex outline-1 cursor-pointer border-[1px] border-purple-700 px-4 py-[4px] rounded-lg font-semibold text-[14px] hover:bg-purple-700 hover:text-white transition delay-100">Login</div>
+                <div className="flex outline-1 cursor-pointer bg-purple-600 text-white border-[1px] px-4 py-[4px] rounded-lg font-semibold text-[14px] hover:border-purple-700 hover:text-black hover:bg-white transition delay-100">Sign up</div>
+            </div>
         </div>
      );
 }
